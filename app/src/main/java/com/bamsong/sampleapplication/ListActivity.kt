b3370fce253264bc4f25477765da6f32923403f1
Daniel <dev.bamssong@gmail.com>
2022-11-28 14:34:59 +0900

    목록 화면 생성

--- app/src/main/java/com/bamsong/sampleapplication/ListActivity.kt
+++ app/src/main/java/com/bamsong/sampleapplication/ListActivity.kt
@@ -1,24 +1,18 @@
 package com.bamsong.sampleapplication
 
-import android.content.Intent
-import androidx.compose.ui.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.bamsong.sampleapplication.ui.theme.SampleApplicationTheme
 
-class MainActivity : ComponentActivity() {
+class ListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -27,8 +21,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    SampleCard("메인 화면 입니다.") {
-                        startActivity(Intent(this@MainActivity,ListActivity::class.java))
+                    SampleCard("목록", color = Color.Green) {
+                        //nothing.
                     }
                 }
             }
@@ -36,26 +30,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun SampleCard(text: String,
-               color: Color = Color.Gray,
-               onClick: () -> Unit) {
-    Box(modifier = Modifier
-        .clickable(onClick = onClick)
-        .fillMaxSize()
-        .background(color),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = text
-        )
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ListPreview() {
     SampleApplicationTheme {
-        SampleCard("메인 화면 입니다.") { }
+        SampleCard("목록!!", color = Color.Green) { }
     }
 }
